a247d79a375ca2a492ae0794bd169bc5c0f464ca
KMotiour <kazi_motiour@coredevs.co>
2022-02-25 00:13:00 +0600

    setup for deployment

--- imagined_search/settings.py
+++ imagined_search/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#9^92@r$vna!(jd@mzbu2df#7=gf%=qf$0nzd#d!&a=5+15@)2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['imagined-search.herokuapp.com','172.0.0.1']
 
 
 # Application definition
