31d1968d65603e16509762011f1e20d889a4e9df
KMotiour <kazi_motiour@coredevs.co>
2022-02-25 01:04:02 +0600

    DEBUG=TURE

--- searchResults/urls.py
+++ searchResults/urls.py
@@ -4,9 +4,8 @@ from django.contrib.auth import views
 app_name='search'
 
 urlpatterns = [
-    # Authentication
     path('', searchFormGoogle, name='search_google'),
     path('search-history/', gets_filtered_items, name='searchHistory'),
     path('filter-resuls/', filter_search_resuls, name='filterResuls')
   
-]
+]   
